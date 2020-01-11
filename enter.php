c930066747ad509377c7775c365b537f3c86737c
macloo <mmcadams@gmail.com>
2020-01-11 15:56:34 -0500

    minor edits

--- enter.php
+++ enter.php
@@ -1,8 +1,18 @@
-<?php include 'database.php'; ?>
-
 <?php
-// This is the "prepared statement" version of this file.
 
+include 'database.php';
+
+// use of prepared statements - SQL
+
+// erase any HTML tags and then escape all quotes
+// this is used on each value that came from the HTML form
+function sanitizeMySQL($conn, $var) {
+    $var = strip_tags($var);
+    $var = mysqli_real_escape_string($conn, $var);
+    return $var;
+}
+
+// ensure that form values were received
 if (isset($_POST['name']) && isset($_POST['style'])) {
 
     // sanitizeMySQL() is a custom function, written below
@@ -47,13 +57,5 @@ if (isset($_POST['name']) && isset($_POST['style'])) {
 } else {
     echo "Failed to enter!";
 } // end of isset if-statement
-
-// erase any HTML tags and then escape all quotes
-// this is used on each value that came from the HTML form 
-function sanitizeMySQL($conn, $var) {
-    $var = strip_tags($var);
-    $var = mysqli_real_escape_string($conn, $var);
-    return $var;
-}
 
 ?>

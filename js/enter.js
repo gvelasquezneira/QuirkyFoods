d3b35be51e345bfe4d8c29f83c784440ba5fe06f
macloo <mmcadams@gmail.com>
2020-01-11 16:37:05 -0500

    killed jQuery + added fetch

--- js/enter.js
+++ js/enter.js
@@ -1,28 +1,45 @@
-$(document).ready(function() {
+/* this file contains one event listener for enter_new_record.php.
+    the JavaScript here acts as a bridge between the HTML form
+    in enter_new_record.php and the PHP script in enter.php -
+    which writes the new record into the database.
+*/
 
-  $("#response").hide();
+// this is the form in enter_new_record.php -
+const sockform = document.querySelector('#sockform');
 
-  $("#sockform").on("submit", function(e) {
-    e.preventDefault();
+// this is a DIV in enter_new_record.php -
+const response_content = document.querySelector('#response');
 
-    $.ajax({
-      url: "enter.php",
-      type: "POST",
-      data: $(this).serialize(),
-      success: function(response) {
-        $("#socks").hide();
-        $("#response").show();
-      },
-      error: function(xhr, status, err) {
-        alert("Error! Message from server: " + xhr.status + " " + err);
-      }
-      // --- data: $(this).serialize(), ---
-      // takes the form data and puts all of it into a single string
-      // that the PHP script can read - NOTE - requires
-      // a unique NAME attribute for every form element or it
-      // WILL NOT WORK
-    });
+// this DIV contains the form in enter_new_record.php -
+const socks_form = document.querySelector('#socks');
+// show the DIV if it is hidden
+socks_form.style.display = 'block';
 
-  });
 
-}); // close document ready
+// event handler
+sockform.onsubmit = (e) => {
+    // gets the names and current values from the form
+    const formData = new FormData(sockform);
+    // FETCH
+    fetch('enter.php', {
+        method: "POST",
+        body: formData,
+        credentials: "same-origin"
+    })
+    .then( (response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.text();
+    })
+    .then( (data) => {
+        socks_form.style.display = 'none';
+        response_content.innerHTML =
+        "<p class='announce'>" + data + "</p>" +
+        "<p class='middle'><a href='enter_new_record.php'>Return to the form</a></p>";
+    })
+    .catch( (error) => {
+        console.error('Error in fetch: ', error);
+    }); // end of FETCH
+    return false;
+}; // end sockform.onsubmit

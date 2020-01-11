82bb9b25f8f8ec04d5ef5a39e3f288b9249f586b
macloo <mmcadams@gmail.com>
2020-01-11 15:27:56 -0500

    minor edits

--- inventory_update.php
+++ inventory_update.php
@@ -24,8 +24,11 @@
 
 <p class="middle">Then click the Submit button at the bottom of the table.</p>
 
-<!-- this form is handled by the PHP file named in the action attribute -->
+<!--
+     the form below is handled by the PHP file named in the action= attribute
+-->
 <form class="smallform" method="post" action="socks_edit.php" autocomplete="off">
+	
 <table>
 	<!-- table column headings -->
 	<tr>
@@ -40,20 +43,25 @@
 
 <!-- Begin PHP while-loop to display database query results
      with each row enclosed in TD tags.
-	   Each time it loops, it writes ONE ROW -->
+     Each time it loops, it writes ONE ROW.
+	 This code depends on the first 5 lines at the start of this file.
+	 $socks comes from that code.
+	 NOTE all form controls must have a name= attribute.
+     -->
 <?php while ($row = mysqli_fetch_assoc($socks)) :  ?>
 
 <tr>
-  <td><input type="radio" name="id" id="<?php echo $row['id']; ?>" value="<?php echo $row['id']; ?>"></td>
-	<!-- notice how, above, the database record id becomes
-		 the id and value of the radio button -->
+  <td><input type="radio" name="id" id="<?php echo $row['id']; ?>"
+	   value="<?php echo $row['id']; ?>"></td>
+  <!-- notice how, above, the database record id becomes
+       the id and value of the radio button -->
   <td><?php echo stripslashes($row['name']); ?></td>
   <td><?php echo $row['style']; ?></td>
   <td><?php echo $row['color']; ?></td>
   <td><?php echo $row['quantity']; ?></td>
   <td><?php echo $row['price']; ?></td>
   <td><?php echo $row['updated']; ?></td>
-</tr>
+</tr><!-- end of HTML table row -->
 
 <?php endwhile;  ?>
 <!-- end the PHP while-loop
@@ -62,8 +70,9 @@
 </table>
 
 <input type="submit" id="submit" value="Submit One Row for Editing">
-</form>
+
 <!-- close the form -->
+</form>
 
 <p class="middle"><a href="enter_new_record.php">Add a new sock record</a></p>
 
